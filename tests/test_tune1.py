c29b5037237869fbe79960d217b712de21cfd4ca
korn3lia <64958573+korn3lia@users.noreply.github.com>
2020-06-05 11:55:05 +0200

    Update test_tune1.py

--- tests/test_tune1.py
+++ tests/test_tune1.py
@@ -5,7 +5,7 @@
 
 
 import music
-
+#the music module doesn't work but the main focus is the sample tune
 
 tune = ["C4:4", "D4:4", "E4:4", "C4:4", "C4:4", "D4:4", "E4:4", "C4:4",
         "E4:4", "F4:4", "G4:8", "E4:4", "F4:4", "G4:8"]
